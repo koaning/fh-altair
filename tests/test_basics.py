2d02bd879ba89eeb2d164badbf0437f75251c725
Daniel Tan <danieltan.singapore@gmail.com>
2024-10-03 21:18:55 +0800

    Autofmt

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,8 +1,15 @@
-import pandas as pd
 import altair as alt
+import pandas as pd
+
 from fh_altair import altair2fasthtml
 
+
 def test_no_err():
-    pltr = pd.DataFrame({'y': [1, 2, 3, 2], 'x': [3, 1, 2, 4]})
-    chart = alt.Chart(pltr).mark_line().encode(x='x', y='y').properties(width=400, height=200)
+    pltr = pd.DataFrame({"y": [1, 2, 3, 2], "x": [3, 1, 2, 4]})
+    chart = (
+        alt.Chart(pltr)
+        .mark_line()
+        .encode(x="x", y="y")
+        .properties(width=400, height=200)
+    )
     return altair2fasthtml(chart)
