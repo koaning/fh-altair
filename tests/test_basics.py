2529cc01e4c8f24e4b1d2a20e111ed169225c781
Daniel Tan <danieltan.singapore@gmail.com>
2024-10-03 21:19:14 +0800

    Add test for vega_options

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -13,3 +13,17 @@ def test_no_err():
         .properties(width=400, height=200)
     )
     return altair2fasthtml(chart)
+
+
+def test_vega_options_passed_to_vegaEmbed():
+    pltr = pd.DataFrame({"y": [1, 2, 3, 2], "x": [3, 1, 2, 4]})
+    chart = (
+        alt.Chart(pltr)
+        .mark_line()
+        .encode(x="x", y="y")
+        .properties(width=400, height=200)
+    )
+    vega_embed_call = altair2fasthtml(
+        chart, vega_options={"renderer": "svg", "actions": True}
+    ).__str__()
+    assert '{"renderer": "svg", "actions": true}' in vega_embed_call
