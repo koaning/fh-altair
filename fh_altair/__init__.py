6bca08bc7c42c54fb681422f2365df14f770d86b
Daniel Tan <danieltan.singapore@gmail.com>
2024-10-03 21:16:46 +0800

    Autofmt

--- fh_altair/__init__.py
+++ fh_altair/__init__.py
@@ -1,17 +1,19 @@
 from uuid import uuid4
-from fasthtml.common import Div, Script
 
+from fasthtml.common import Div, Script
 
 altair_headers = [
     Script(src="https://cdn.jsdelivr.net/npm/vega@5"),
     Script(src="https://cdn.jsdelivr.net/npm/vega-lite@5"),
-    Script(src="https://cdn.jsdelivr.net/npm/vega-embed@6")
+    Script(src="https://cdn.jsdelivr.net/npm/vega-embed@6"),
 ]
 
 
 def altair2fasthml(chart):
     """This is the version with bad spelling"""
-    print("You have imported `altair2fasthml` which is a misspelled function. Sorry about that! It will be deprecated in favour of `altair2fasthtml` in a later release.")
+    print(
+        "You have imported `altair2fasthml` which is a misspelled function. Sorry about that! It will be deprecated in favour of `altair2fasthtml` in a later release."
+    )
     return altair2fasthtml(chart)
 
 
